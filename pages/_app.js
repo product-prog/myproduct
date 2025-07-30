ee893b4ff68b4a66d02b859cda46eca8e0c243c0
Estoms <enagnonattindogbe@gmail.com>
2025-07-30 18:48:30 +0100

    Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -3,3 +3,17 @@ import "@/styles/globals.css";
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
+// pages/_app.js
+import { Analytics } from '@vercel/analytics/react'; 
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
+}
+
+export default MyApp;
